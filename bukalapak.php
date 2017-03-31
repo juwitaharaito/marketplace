ad1bcbc648895b1e6cabf643da32bb6257354ea0
Juwita Pebriana Pasaribu <juwitaharaito@gmail.com>
2017-03-31 11:56:39 +0700

    Update bukalapak.php
    cek redirect karena untuk keyword asus ternyata di redirect. tapi preg-match nya masih belum bener

--- bukalapak.php
+++ bukalapak.php
@@ -21,7 +21,7 @@
 			$indexkeyword++;
 		}
 		
-		if ($statu == 'menu') //berarti pindah menu. keyword dari textfield
+		if ($statu == 'menu' || $statu == 'kategori') //berarti pindah menu. keyword dari textfield
 		{
 			if ($indexkeywords<0) //berarti nggak ada kecocokan kata
 			{
@@ -43,9 +43,8 @@
 					$new = true;
 					$kets = "keyword ada tapi kategori nggak, page = ".$page;
 				}
-				else
+				else //berarti kategori ada
 				{
-					
 					$page =  count($json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['hal']); 
 					$kets = "keyword dan kategori ada, page = ".$page;
 					$new = false; //new = false, dia cuma menampilkan yang pernah dibuka					
@@ -66,19 +65,34 @@
 				$tambahan = '&search%5Bsort_by%5D=price%3Aasc';						
 			else
 				$tambahan = '&search%5Bsort_by%5D=last_relist_at%3Adesc';
+			$ch2 = curl_init('https://www.bukalapak.com/products?&search%5Bkeywords%5D='.$strings);
+			curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+			curl_exec($ch2);
+			if (!curl_errno($ch2)) 
+			{
+				$info = curl_getinfo($ch2);
+				if ($info['redirect_url']!="")
+					$redirect = $info['redirect_url'].'?'.$tambahan.$pageurl;
+				else
+					$redirect = $info['url'].$tambahan.$pageurl;
+			}
+			if ($redirect == "")
+				$redirect = 'https://www.bukalapak.com/products?&search%5Bkeywords%5D='.$strings.$tambahan.$pageurl;
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_URL,'https://www.bukalapak.com/products?&search%5Bkeywords%5D='.$strings.$tambahan.$pageurl);
+			curl_setopt($ch, CURLOPT_URL, $redirect);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 			$result=curl_exec($ch);
 			curl_close($ch);
+			curl_close($ch2);
 			$pecah = explode("<ul class='products row-grid' data-module='product-list'>",$result);
 			$pecah2 = explode ("</ul>",$pecah[1]);
 			$string = "|<[^>]+>(.*)</[^>]+>|U";
 			preg_match_all($string, $pecah2[0], $match, PREG_PATTERN_ORDER);
 			$json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['hal'][$page-1]['items'] = [];
-			$i=0; $a=0; $b=0;
+			$i=0; $a=0; $b=0; $c=0;
 			foreach ($match[0] as $temp)
 			{
 				preg_match('/width="194" height="194" alt="(.+?)" data-src=/',$temp,$temp2);
@@ -102,8 +116,27 @@
 					$json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['hal'][$page-1]['items'][$b]['harga']= $temp4[1];
 					$b++;
 				}
-			
-	
+				preg_match('/class="product-media__link js-tracker-product-link" href="(.+?)">/',$temp,$temp5);
+				if ($temp5[1] != "")
+				{
+					$json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['hal'][$page-1]['items'][$c]['url']= 'https://www.bukalapak.com'.$temp5[1];
+					$c++;
+				}
+			}
+			if ($json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['pagecount']== null)
+			{
+				preg_match('/<span class="last-page">(.+?)<\/span>/',$result,$pagecount);
+				if ($pagecount[1] != null)
+					$pagecounts = $pagecount[1];
+				else
+				{
+					preg_match('/">(.+?)<\/a> <a class="next_page"/',$result,$pagecount);
+					if ($pagecount[1] != null)
+						$pagecounts = substr($pagecount[1], -1);
+					else
+						$pagecounts = '1';
+				}
+				$json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['pagecount'] = $pagecounts;
 			}
 			$ket = "keyword tidak ada atau kategori tidak ada atau tekan loadmore";
 		}
@@ -117,12 +150,14 @@
 			{
 				$array[$j]->gambar =  $items['gambar'];
 				$array[$j]->nama = $items['nama'];
+				$array[$j]->url = $items['url'];
 				$array[$j]->harga = "Rp. ".$items['harga'];	
 				$j++;
 			}
 		}
-
-		echo json_encode(array("arr" => $array, "mp" => "2", "json" => $json, "link" => $redirect, "kata" => $kata, "kategori" => $kategori, "page" =>$page));
+		//echo $redirect;
+		$pagecounts = $json[2]['katakunci'][$indexkeywords]['kategori'][$kategori]['pagecount'];
+		echo json_encode(array("arr" => $array, "mp" => "2", "link" => $redirect, "json" => $json, "kasus" => '1', "kata" => $kata, "kategori" => $kategori, "page" =>$page, "pagecount" => $pagecounts));
 		
 	}
 	else 
